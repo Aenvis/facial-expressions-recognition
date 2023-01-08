096016c5d003cb52f059bf998235cf2671d3689a
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-01-08 16:29:37 +0100

    Add load_data function to train.py and it loads image datasets

--- src/train.py
+++ src/train.py
@@ -1,2 +1,27 @@
 import tensorflow as tf
 import pandas as pd
+
+
+def load_data(train_path: str, test_path: str) -> None:
+    # load train dataset
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        train_path,
+        image_size=(48, 48),
+        batch_size=64,
+        color_mode='grayscale',
+        label_mode='categorical'
+    )
+
+    # load test dataset
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+        test_path,
+        image_size=(48, 48),
+        batch_size=64,
+        color_mode='grayscale',
+        label_mode='categorical'
+    )
+
+    print(train_ds.class_names)
+    print(test_ds.class_names)
+
+load_data('data/train', 'data/test') 
