78fba37e9d40c02dd4c15b9e8f016c7d2f0f64c1
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-01-09 21:05:32 +0100

    Add trained model saved as model.h5 - acc 0.85

--- src/train.py
+++ src/train.py
@@ -1,27 +1,53 @@
 import tensorflow as tf
 import pandas as pd
 
+train_path = '../data/train'
+test_path = '../data/test'
 
-def load_data(train_path: str, test_path: str) -> None:
-    # load train dataset
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        train_path,
-        image_size=(48, 48),
-        batch_size=64,
-        color_mode='grayscale',
-        label_mode='categorical'
-    )
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
+# load train dataset
+train_ds = train_datagen.flow_from_directory(
+train_path,
+target_size=(48, 48),
+batch_size=64,
+color_mode='grayscale',
+class_mode='categorical'
+)
 
-    # load test dataset
-    test_ds = tf.keras.utils.image_dataset_from_directory(
-        test_path,
-        image_size=(48, 48),
-        batch_size=64,
-        color_mode='grayscale',
-        label_mode='categorical'
-    )
+test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
+# load test dataset
+test_ds = test_datagen.flow_from_directory(
+test_path,
+target_size=(48, 48),
+batch_size=64,
+color_mode='grayscale',
+class_mode='categorical'
+)
 
-    print(train_ds.class_names)
-    print(test_ds.class_names)
+model = tf.keras.Sequential([ 
+    tf.keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(48, 48, 1)),
+    tf.keras.layers.Conv2D(64, kernel_size=(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Conv2D(128, kernel_size=(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(128, kernel_size=(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(1024, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(7, activation='softmax')
+])
 
-load_data('data/train', 'data/test') 
+model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001, weight_decay=1e-6), metrics=['accuracy'])
+model_info = model.fit(
+    train_ds,
+    steps_per_epoch=28709 // 64,
+    epochs=50,
+    validation_data=test_ds,
+    validation_steps=7178 // 64
+)
+
+model.save_weights('../data/model.h5')
+model.save_weights('../data/model1.h5')
